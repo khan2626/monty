15924765cce8b6816c6502df96c689984ead71f2
khan2626 <israelkhan217@gmail.com>
2023-08-23 23:51:59 +0100

    nop.c

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - it does nothing
+ * @head: top element of stack
+ * @count: line_number
+ * Return: Nothing
+ */
+void nop(stack_t **head, unsigned int counter)
+{
+  (void) count;
+  (void) head;
+}
